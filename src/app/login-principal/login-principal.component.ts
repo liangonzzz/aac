28c40fa7dfbddaf8cdc74ff699752f3d9e42e6ca
Hector Mauricio Coronado Gutierrez <hector.coronado@dcsas.com.co>
2025-01-30 16:53:40 -0500

    Se corrige problema de botón

--- src/app/login-principal/login-principal.component.ts
+++ src/app/login-principal/login-principal.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 // prime
@@ -10,22 +10,24 @@ import { ButtonModule } from 'primeng/button';
 import { DropdownModule } from 'primeng/dropdown';
 import { CheckboxModule } from 'primeng/checkbox';
 
+
 @Component({
   selector: 'app-login-principal',
   standalone: true,
   imports: [
     CommonModule,
-    FormsModule,
-    InputTextModule,
+     InputTextModule,
+     ReactiveFormsModule, 
     PasswordModule,
     ButtonModule,
     DropdownModule,
     CheckboxModule
   ],
   templateUrl: './login-principal.component.html',
-  styleUrls: ['./login-principal.component.css']
+  styleUrls: ['./login-principal.component.scss']
 })
 export class LoginPrincipalComponent {
+  loginForm: FormGroup;
   documentTypes = [
     { label: 'Cédula de Ciudadanía', value: 'cc' },
     { label: 'Cédula de Extranjería', value: 'ce' }
@@ -34,11 +36,21 @@ export class LoginPrincipalComponent {
   selectedDocumentType: string | null = null;
   password: string = '';  // Definir la propiedad para la contraseña
 
-  constructor(private router: Router) {}
+  constructor(private router: Router,  private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      documentType: [null, Validators.required],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
 
+ 
   onSubmit() {
     // Aquí puedes agregar la lógica de autenticación si es necesario
-    this.router.navigate(['/pantalla-creacion']);
+    if (this.loginForm.valid) {
+        this.router.navigate(['/pantalla-creacion']);
+    } else {
+      console.log('Formulario inválido');
+    }
   }
 
   navigateToLoginRestablecer() {
