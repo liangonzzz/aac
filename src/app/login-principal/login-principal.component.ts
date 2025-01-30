dc09dd981608f29266a29c5e5b2bfbf18b741e2b
liangonzzz <lian.rippeth@gmail.com>
2025-01-30 17:09:05 -0500

    edicion
    edicion

--- src/app/login-principal/login-principal.component.ts
+++ src/app/login-principal/login-principal.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { AuthService } from '../service/auth.service'; // Importa el servicio de autenticación correctamente
 
 // prime
 import { InputTextModule } from 'primeng/inputtext';
@@ -28,17 +29,47 @@ import { CheckboxModule } from 'primeng/checkbox';
 export class LoginPrincipalComponent {
   documentTypes = [
     { label: 'Cédula de Ciudadanía', value: 'cc' },
-    { label: 'Cédula de Extranjería', value: 'ce' }
+    { label: 'Cédula de Extranjería', value: 'ce' },
+    { label: 'Pasaporte', value: 'pt' }
   ];
 
-  selectedDocumentType: string | null = null;
-  password: string = '';  // Definir la propiedad para la contraseña
+  selectedDocumentType: { label: string, value: string } | null = null;
+  documentNumber: string = '';
+  password: string = '';
+  errorMessage: string = '';
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private authService: AuthService) { } // Inyecta el servicio de autenticación
 
   onSubmit() {
-    // Aquí puedes agregar la lógica de autenticación si es necesario
-    this.router.navigate(['/pantalla-creacion']);
+    console.log('Document Type:', this.selectedDocumentType?.value);
+    console.log('Document Number:', this.documentNumber);
+    console.log('Password:', this.password);
+
+    if (this.selectedDocumentType && this.documentNumber && this.password) {
+      this.authService.login(this.selectedDocumentType.value, this.documentNumber, this.password).subscribe(
+        (isAuthenticated: boolean) => { // Especifica el tipo boolean
+          console.log('Authentication Result:', isAuthenticated);
+          if (isAuthenticated) {
+            this.router.navigate(['//pantalla-creacion']);
+          } else {
+            this.errorMessage = 'Tipo de documento, número de identificación o contraseña incorrectos.';
+          }
+        },
+        (error: any) => { // Especifica el tipo any
+          console.error('Error:', error);
+          this.errorMessage = 'Error al cargar los datos de usuario.';
+        }
+      );
+    } else {
+      this.errorMessage = 'Por favor, complete todos los campos.';
+    }
+    setTimeout(() => {
+      this.errorMessage = '';
+    }, 1500);
+  }
+
+  onDocumentTypeChange() {
+    console.log('Document Type Changed:', this.selectedDocumentType);
   }
 
   navigateToLoginRestablecer() {
