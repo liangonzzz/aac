c2782c3219f3323cfacb30f1e6af76569f150b9c
liangonzzz <lian.rippeth@gmail.com>
2025-02-11 17:26:07 -0500

    <
    a

--- src/app/hola/hola.component.ts
+++ src/app/hola/hola.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InputLoginComponent } from '../shared/input-login/input-login.component';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-hola',
@@ -14,10 +14,19 @@ export class HolaComponent {
   loginForm: FormGroup;
 
   constructor(private fb: FormBuilder) {
-    this.loginForm = this.fb.group({
-      usuario: ['', Validators.required],
-      password: ['', Validators.required]
+    this.loginForm = new FormGroup({
+      usuario: new FormControl('', Validators.required),
+      password: new FormControl('', Validators.required)
     });
+  }
+
+  // Getters para acceder a los controles como FormControl directamente
+  get usuarioControl(): FormControl {
+    return this.loginForm.get('usuario') as FormControl;
+  }
+
+  get passwordControl(): FormControl {
+    return this.loginForm.get('password') as FormControl;
   }
 
   onSubmit() {
